87482dd8a831a38569a3f5b046f42869ab038eaf
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2025-07-22 01:31:13 +0500

    Update header.php

--- admin/header.php
+++ admin/header.php
@@ -1,8 +1,10 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 if (!isset($_SESSION['admin_id']) && basename($_SERVER['PHP_SELF']) != 'login.php') {
-  header("Location: login.php");
-  exit;
+    header("Location: login.php");
+    exit;
 }
 ?>
 
